c78047657fd8f3e69d5bb7456e5d04ced7ddf786
darrynjordan <jrddar001@myuct.ac.za>
2016-12-01 13:03:41 +0200

    fixed typo in ramp parameter calculator

--- ramp_parameters/matlab_calculations/ramp_calculations.m
+++ ramp_parameters/matlab_calculations/ramp_calculations.m
@@ -4,7 +4,6 @@ s1_inc = fn_end/LEN;
 fn_start = getFracNum(1);
 T = 100;
 F = T*s1_inc - fn_start;
-s2_fn_end = fn_end - F;
 s2_inc = (fn_end - fn_start - F)/(LEN - T);
 
 if (s1_inc == s2_inc)
@@ -13,6 +12,8 @@ else
     fprintf('WARNING: Gradient criteria NOT satisfied.\n');
 end;
 
+fprintf('\nT: \t%f\n', (LEN*(F + fn_start))/fn_end);
+fprintf('F: \t%f\n', F);
 
 fprintf('\nSynthesizer 1:\n');
 fprintf('SRT: \t%f\n', 0);
@@ -23,9 +24,7 @@ fprintf('BW: \t%f\n', getVco(fn_end));
 
 fprintf('\nSynthesizer 2:\n');
 fprintf('SRT: \t%f\n', fn_start);
-fprintf('END: \t%f\n', s2_fn_end - F);
+fprintf('END: \t%f\n', fn_end - F);
 fprintf('INC: \t%f\n', s2_inc);
 fprintf('LEN: \t%f\n', LEN - T);
-fprintf('BW: \t%f\n', getVco(s2_fn_end) - getVco(fn_start));
-
-fprintf('\nT-check: \t%f\n', (LEN*(F + fn_start))/fn_end);
+fprintf('BW: \t%f\n', getVco(fn_end) - getVco(fn_start));
