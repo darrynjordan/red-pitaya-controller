b5a4fedac07c76fafe8bd28852635b676eb7e0dc
darrynjordan <jrddar001@myuct.ac.za>
2017-03-24 13:52:36 +0200

    updated ramps and moved simulation files to dropbox

--- calculations/beatToTime.m
+++
@@ -1,8 +0,0 @@
-function [ time_delay ] = beatToTime( beat_freq, bandwidth, modulation_period )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-time_delay = (beat_freq*modulation_period)/bandwidth;
-
-end
-
